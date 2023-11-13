4ee6d6e982156cc29377ca9dc6f23f28d72a0610
jkdevops.codes <jkdevops.codes@gmail.com>
2023-11-13 11:59:51 +0530

    Edited

--- section13/metrics.sh
+++ section13/metrics.sh
@@ -1,4 +1,11 @@
-# Create a namespace named cpu-example and create multiple pods given in the source code 
+#Make a namespace in Kubernetes called 'cpu-example' and create pods as described in the code found at https://github.com/jkdevops-codes/simply-certify-cka/blob/master/section13/metrics.sh.
+
+#After finishing this, double-check that the Metric Server is installed. If it's not installed yet, follow the setup instructions in section 3.9 at https://github.com/jkdevops-codes/simply-certify-cka/blob/master/section3/3.9/metrics-server.yaml.
+
+#Look at how much memory the pods are using in the 'cpu-example' area, and organize the results based on memory usage.
+
+#Take a look at the logs from the busybox container inside the pod called busy-pod-1, and save what you see into a file named busylogs.txt
+
 k create ns cpu-example
 
 cat <<EOF >> top-test.yaml
@@ -45,6 +52,8 @@ metadata:
   namespace: cpu-example
 spec:
   containers:
+  - name: nginx
+    image: nginx
   - name: busy-container-1
     image: busybox
     command: ["/bin/sh", "-c"]
@@ -57,3 +66,5 @@ spec:
 EOF
 
 k apply -f top-test.yaml
+
+

c27436b73d2326f3be926be39a904201acf91cf3
Kanishkan <kanishkannadaraja4848@gmail.com>
2024-05-28 10:17:50 +0530

    Updated the scripts to support Kubernetes v1.30.

--- section2/2.5/2-install-kube-tools-allvms.sh
+++ section2/2.5/2-install-kube-tools-allvms.sh
@@ -1,11 +1,11 @@
 ##Install kubeadm, kubelet, kubectl 
 sudo apt-get update
-sudo apt-get install -y apt-transport-https ca-certificates curl
+sudo apt-get install -y apt-transport-https ca-certificates curl gpg
 
 
 #Better run below two lines one by one.
-curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.29/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
-echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.28/deb/ /" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.30/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.30/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
 #curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-archive-keyring.gpg
 #echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
@@ -18,11 +18,13 @@ sudo apt-get update
 
 #apt list -a kubeadm | head -5
 
-KUBECOMPNENTS_VERSION=1.29.3-1.1
+KUBECOMPNENTS_VERSION=1.30.1-1.1 
 sudo apt-get install -y kubelet=${KUBECOMPNENTS_VERSION}
 sudo apt-get install -y kubectl=${KUBECOMPNENTS_VERSION} 
 sudo apt-get install -y  kubeadm=${KUBECOMPNENTS_VERSION}
 
-sudo apt-mark hold kubelet #prevent updating the packages automatically
+#prevent updating the packages automatically
+sudo apt-mark hold kubelet
 sudo apt-mark hold kubectl
 sudo apt-mark hold kubeadm
+
